d1face1e84fa555ae7380a3e78dc813fb1106fcf
Edison <edisonj@umich.edu>
2025-05-23 21:30:02 -0400

    Fixed Potential Security Concerns with Collection Dropdown

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,8 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import PreparationController from "./preparation_controller"
+import CollectionDropdownController from "./collection_dropdown_controller"
 
+application.register("collection-dropdown", CollectionDropdownController)
 application.register("preparation", PreparationController)
 eagerLoadControllersFrom("controllers", application)
