2ecea63e9abbc0d51c93ac158c3d0336ffce738c
Margarita Barvinok <brita@umich.edu>
2025-05-24 23:28:04 -0400

    hide options

--- app/javascript/controllers/options_controller.js
+++ app/javascript/controllers/options_controller.js
@@ -2,10 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="options"
 export default class extends Controller {
-  static targets = ["fields", "template"];
+  static targets = ["fields", "show_options", "template", "question_type"];
   
   connect() {
     console.log("Options controller connected");
+  }
+
+  showOptions() {
+    const questionType = this.question_typeTarget.value;
+    console.log("Selected question type:", questionType);
+    if (questionType === "dropdown" || questionType === "checkbox") {
+      this.show_optionsTarget.classList.remove("invisible")
+      this.show_optionsTarget.classList.add("visible")
+    } else {
+      console.log("Hiding options");
+      this.show_optionsTarget.classList.remove("visible")
+      this.show_optionsTarget.classList.add("invisible")
+    }
   }
 
   append() {
