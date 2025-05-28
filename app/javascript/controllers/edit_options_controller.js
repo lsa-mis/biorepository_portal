a62bd9145635afc0131bb106f846cd437495edac
Margarita Barvinok <brita@umich.edu>
2025-05-28 12:50:31 -0400

    add id to buttons

--- app/javascript/controllers/edit_options_controller.js
+++ app/javascript/controllers/edit_options_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit_options"
 export default class extends Controller {
-  static targets = ["fields", "show_options", "template", "question_type", "option_label", "option_value", "option_number"];
+  static targets = ["fields", "show_options", "template", "question_type", "option_label", "option_value", "option_number", "remove_button"];
   
   connect() {
     console.log("Options controller connected");
@@ -12,6 +12,7 @@ export default class extends Controller {
     console.log("append called");
     this.fieldsTarget.insertAdjacentHTML("beforeend", this.#templateContent);
     let thisOptionsNumber = document.querySelectorAll(".option-list-item").length;
+    console.log("thisOptionsNumber", thisOptionsNumber);
 
     let option_label = this.option_labelTarget;
     console.log("option_label", option_label);
@@ -21,9 +22,11 @@ export default class extends Controller {
 
     this.option_valueTarget.setAttribute("id", `options_attributes_${thisOptionsNumber}_option`);
 
+    this.remove_buttonTarget.setAttribute("id", `remove_button_${thisOptionsNumber}`);
+
     this.option_numberTarget.setAttribute("value", thisOptionsNumber);
-    this.option_numberTarget.setAttribute("name", `loan_question[options_attributes][${thisOptionsNumber}][number]`);
-    this.option_numberTarget.setAttribute("id", `loan_question_options_attributes_${thisOptionsNumber}_number`);
+    this.option_numberTarget.setAttribute("name", `option_number_[${thisOptionsNumber}]`);
+    this.option_numberTarget.setAttribute("id", `option_number_${thisOptionsNumber}`);
 
     this.showOnlyLastRemoveOptionButton();
   }
