03524ece6822d7a1b6d03069776852967fbe25c4
Clarissa <yiman@umich.edu>
2025-06-09 12:34:39 -0400

    add-validations-to-LoanQuestion done

--- app/models/loan_question.rb
+++ app/models/loan_question.rb
@@ -10,9 +10,23 @@
 #  updated_at    :datetime         not null
 #
 class LoanQuestion < ApplicationRecord
-  has_many :options, dependent: :destroy
-  accepts_nested_attributes_for :options, allow_destroy: true
+ has_many :options, dependent: :destroy
+ accepts_nested_attributes_for :options, allow_destroy: true
+ enum :question_type, [:text, :dropdown, :checkbox], prefix: true
 
-  enum :question_type, [:text, :dropdown, :checkbox], prefix: true
-  validates :question, presence: true
-end
+
+ validates :question, presence: true, uniqueness: true
+ validates :question_type, presence: true
+ validate :must_have_multiple_options_if_needed
+
+ private
+
+ def must_have_multiple_options_if_needed
+   if question_type.in?(%w[dropdown checkbox])
+     option_count = options.reject(&:marked_for_destruction?).size
+     if option_count < 2
+       errors.add(:options, "must have at least two options for dropdown or checkbox question types")
+     end
+   end
+ end
+ end
