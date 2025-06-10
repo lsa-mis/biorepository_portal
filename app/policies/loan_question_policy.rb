1acb4d26696282836486af5f554bc7b411b15600
Margarita Barvinok <brita@umich.edu>
2025-06-10 10:11:37 -0400

    edit format

--- app/policies/loan_question_policy.rb
+++ app/policies/loan_question_policy.rb
@@ -17,12 +17,15 @@ class LoanQuestionPolicy < ApplicationPolicy
   def show?
     is_admin?
   end
+
   def edit?
     update?
   end
+
   def update?
     is_super_admin?
   end
+  
   def destroy?
     is_super_admin?
   end
