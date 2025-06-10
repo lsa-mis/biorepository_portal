41705f783a7845b9ce00cf13c8f3a19cf98044ef
Margarita Barvinok <brita@umich.edu>
2025-06-10 10:32:35 -0400

    add faq policy tests

---
+++ spec/policies/faq_policy_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe FaqPolicy, type: :policy do
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:faq) { FactoryBot.create(:faq) }
+
+  describe 'permissions by role' do
+    context 'developer' do
+      subject { described_class.new({ user: user, role: "developer" }, faq) }
+
+      it { is_expected.to forbid_actions(%i[]) }
+      it { is_expected.to permit_actions(%i[index show create new update edit destroy]) }
+    end
+
+    context 'super_admin' do
+      subject { described_class.new({ user: user, role: "super_admin" }, faq) }
+
+      it { is_expected.to forbid_actions(%i[]) }
+      it { is_expected.to permit_actions(%i[index show create new update edit destroy]) }
+    end
+
+    context 'admin' do
+      subject { described_class.new({ user: user, role: "admin" }, faq) }
+
+      it { is_expected.to forbid_actions(%i[]) }
+      it { is_expected.to permit_actions(%i[index show create new update edit destroy]) }
+    end
+
+    context 'user' do
+      subject { described_class.new({ user: user, role: "user" }, faq) }
+
+      it { is_expected.to permit_actions(%i[index show ]) }
+      it { is_expected.to forbid_actions(%i[create new update edit destroy]) }
+    end
+
+    context 'none' do
+      subject { described_class.new({ user: user, role: "none" }, faq) }
+
+      it { is_expected.to permit_actions(%i[index show ]) }
+      it { is_expected.to forbid_actions(%i[create new update edit destroy]) }
+    end
+  end
+end
