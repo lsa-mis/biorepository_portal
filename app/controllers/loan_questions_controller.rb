d75fb62be879661ab65c0344c87a4bf7d19b6503
Edison <edisonj@umich.edu>
2025-06-13 02:16:17 -0400

    Modified model and controller

--- app/controllers/loan_questions_controller.rb
+++ app/controllers/loan_questions_controller.rb
@@ -111,6 +111,7 @@ class LoanQuestionsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def loan_question_params
-      params.require(:loan_question).permit(:question, :question_type, :required, options_attributes: [:id, :value, :_destroy])
+      params.expect(loan_question: [ :question, :question_type, :required, :attachment, options: [ :value ] ])
     end
 end
+
