5a71ae864896894d5794728cb9b4f83a6e92acd7
Margarita Barvinok <brita@umich.edu>
2025-06-15 16:51:19 -0400

    remove duplacates

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,7 +6,7 @@ class RequestsController < ApplicationController
     emails = AppPreference.joins(:collection).where(name: "collection_email_to_send_requests").where.not(value: [nil, '']).pluck("collections.division", :value)
     emails.each { |division, email| @send_to[division] = email }
     generic_email = AppPreference.find_by(name: "generic_contact_email")&.value || ""
-    generic_contact = AppPreference.find_by(name: "generic_contact_email")&.value if generic_email.present?
+    generic_contact = generic_email if generic_email.present?
     @send_to["Collections email"] = generic_contact if generic_email.present?
   end
 
