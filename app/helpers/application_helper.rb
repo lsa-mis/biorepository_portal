ce645eab9ced9945334da573cfbc73ef39ac6fc2
Margarita Barvinok <brita@umich.edu>
2025-06-16 00:18:12 -0400

    simplify method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,11 +78,7 @@ module ApplicationHelper
   def show_count(preparation)
     collection = preparation.item.collection
     max_number_of_preparations = AppPreference.find_by(name: "max_number_of_preparations", collection_id: collection.id)&.value.to_i || 0
-    if max_number_of_preparations > 0 && preparation.count > max_number_of_preparations
-      max_number_of_preparations
-    else
-      preparation.count
-    end
+    max_number_of_preparations > 0 ? [preparation.count, max_number_of_preparations].min : preparation.count
   end
   
 end
