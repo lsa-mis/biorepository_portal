ff8cb04302c61d551dd644044c5d70119d470dab
Margarita Barvinok <brita@umich.edu>
2025-06-20 14:29:45 -0400

    update item cards

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -10,12 +10,13 @@ class CheckoutController < ApplicationController
     count = params[:count].to_i
     current_requestable = @checkout.requestables.find_by(preparation_id: @preparation.id)
     if current_requestable && count > 0
-      current_requestable.update(count:)
+      current_requestable.update(count: current_requestable.count + count)
     elsif count <= 0
       current_requestable.destroy
     else
       @checkout.requestables.create(preparation: @preparation, count:)
     end
+    @max_number_of_preparations = fetch_max_number_of_preparations(@preparation.item.collection.id)
 
     respond_to do |format|
       format.turbo_stream do
@@ -25,7 +26,12 @@ class CheckoutController < ApplicationController
                                                    locals: { checkout: @checkout }),
                               turbo_stream.replace(@preparation),
                               turbo_stream.update('total', partial: 'checkout/total'),
-                              turbo_stream.update('flash', partial: 'layouts/flash')]
+                              turbo_stream.update('flash', partial: 'layouts/flash'),
+                              turbo_stream.update(
+                                "checkout_item_#{@preparation.item_id}",
+                                partial: 'collections/item_card',
+                                locals: { item: @preparation.item }
+                              )]
       end
     end
   end
