3ec2496c9c42cfe53b0eec17bc8d1a90c061cb1c
Margarita Barvinok <brita@umich.edu>
2025-07-05 18:23:59 -0400

    do not need this

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -35,6 +35,5 @@ FactoryBot.define do
     password { Faker::Internet.password(min_length: 10) }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    display_name { "#{first_name} #{last_name}" }
   end
 end
