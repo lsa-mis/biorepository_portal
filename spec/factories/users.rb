0f183699ce3d0d1c0a7be84784c5d7ebe3b2d4d4
Margarita Barvinok <brita@umich.edu>
2025-07-05 17:29:32 -0400

    edit factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -35,5 +35,6 @@ FactoryBot.define do
     password { Faker::Internet.password(min_length: 10) }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    display_name { "#{first_name} #{last_name}" }
   end
 end
