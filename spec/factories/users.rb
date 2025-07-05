2e5aad4a6c240f6cc9a46cf1b94b4b1221188abc
Margarita Barvinok <brita@umich.edu>
2025-07-05 17:02:27 -0400

    fix tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -33,6 +33,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 10) }
-    display_name { Faker::Name.name }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
