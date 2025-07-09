680b0ad15be17f35de1dc743ed82c83b0399356d
Margarita Barvinok <brita@umich.edu>
2025-07-09 08:31:57 -0400

    add console log

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -141,12 +141,14 @@ export default class extends Controller {
       // Add hidden inputs for each field/value (as array if multiple)
       Object.entries(fieldMap).forEach(([fieldName, values]) => {
         if (values.length === 1) {
+          console.log("Adding single value for field");
           const input = document.createElement("input");
           input.type = "hidden";
           input.name = `q[groupings][${groupIndex}][${fieldName}]`;
           input.value = values[0];
           form.appendChild(input);
         } else {
+          console.log("Adding array");
           values.forEach(val => {
             const input = document.createElement("input");
             input.type = "hidden";
