1e06d46a63f5e052c61553395263889241ee26d1
Margarita Barvinok <brita@umich.edu>
2025-07-10 15:24:58 -0400

    edit name

--- app/models/item.rb
+++ app/models/item.rb
@@ -64,7 +64,11 @@ class Item < ApplicationRecord
 
   def name
     # Placeholder for displaying the item name
-    "#{current_identification&.scientific_name} #{current_identification&.vernacular_name}"
+    name = "#{current_identification&.scientific_name.humanize}"
+    if current_identification&.vernacular_name.present?
+      name += " [#{current_identification&.vernacular_name.humanize}]"
+    end
+    name
   end
 
   def coordinates
