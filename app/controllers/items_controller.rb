9aae5daa9c1f7488be19c4f989479b6241d2c816
Margarita Barvinok <brita@umich.edu>
2025-07-14 13:19:40 -0400

    temp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,12 +27,9 @@ class ItemsController < ApplicationController
     end
 
     if params[:q] && params[:q][:groupings] && !params[:page].present?
-
       transformed_groupings = {}
-
       params[:q][:groupings].each do |group_index, group_data|
         group = {}
-
         group_data.each do |field_index, field_data|
           next if field_index == "m"
           next unless field_data["field"].present? && field_data["value"].present?
@@ -46,13 +43,10 @@ class ItemsController < ApplicationController
 
         # Wrap group in an indexed key
         transformed_groupings[group_index] = group
-
         # Add matcher if present
         transformed_groupings[group_index]["m"] = group_data["m"].presence || "or"
       end
-
       params[:q][:groupings] = ActionController::Parameters.new(transformed_groupings).permit!
-      
     end
 
     if params[:q]&.dig(:collection_id_in).present?
@@ -166,6 +160,45 @@ class ItemsController < ApplicationController
     
   end
 
+  def export_to_csv
+    if params[:q] && params[:q][:groupings] && !params[:page].present?
+      transformed_groupings = {}
+      params[:q][:groupings].each do |group_index, group_data|
+        group = {}
+        group_data.each do |field_index, field_data|
+          next if field_index == "m"
+          next unless field_data["field"].present? && field_data["value"].present?
+
+          field = field_data["field"]
+          value = field_data["value"]
+
+          group[field] ||= []
+          group[field] << value
+        end
+
+        # Wrap group in an indexed key
+        transformed_groupings[group_index] = group
+        # Add matcher if present
+        transformed_groupings[group_index]["m"] = group_data["m"].presence || "or"
+      end
+      params[:q][:groupings] = ActionController::Parameters.new(transformed_groupings).permit!
+    end
+    if params[:format] == 'csv'
+      if params[:q].present? 
+        @q = Item.includes(:collection, preparations: :requestables).ransack(params[:q])
+        items = @q.result
+      else
+        items = Item.all
+      end
+      data = data_to_csv(items)
+    end
+
+    respond_to do |format|
+      format.csv { send_data data, filename: "items-#{Date.today}.csv"}
+    end
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
@@ -187,4 +220,48 @@ class ItemsController < ApplicationController
     def search_params
       params.permit(:q)
     end
+
+    def data_to_csv(items = all)
+    item_fields = %w{associated_sequences catalog_number collection_id continent_case_insensitive coordinate_uncertainty_in_meters
+    country_case_insensitive county created_at decimal_latitude decimal_longitude event_date_end event_date_start event_remarks
+    field_number geodetic_datum georeference_protocol georeferenced_by georeferenced_date id individual_count
+    life_stage locality maximum_elevation_in_meters minimum_elevation_in_meters modified occurrence_id
+    occurrence_remarks organism_remarks other_catalog_numbers recorded_by reproductive_condition sampling_protocol
+    sex_case_insensitive state_province_case_insensitive updated_at verbatim_coordinates verbatim_elevation verbatim_event_date verbatim_locality vitality}
+    preparations_fields = %w{prep_type description}
+    identifications_fields = %w{scientific_name vernacular_name order_name family genus}
+    headers = %w{associated_sequences catalog_number collection_id continent_case_insensitive coordinate_uncertainty_in_meters
+    country_case_insensitive county created_at decimal_latitude decimal_longitude event_date_end event_date_start event_remarks
+    field_number geodetic_datum georeference_protocol georeferenced_by georeferenced_date id individual_count
+    life_stage locality maximum_elevation_in_meters minimum_elevation_in_meters modified occurrence_id
+    occurrence_remarks organism_remarks other_catalog_numbers recorded_by reproductive_condition sampling_protocol
+    sex_case_insensitive state_province_case_insensitive updated_at verbatim_coordinates verbatim_elevation verbatim_event_date verbatim_locality vitality
+    prep_type description scientific_name vernacular_name order_name family genus}
+    CSV.generate(headers: true) do |csv|
+      csv << headers
+      items.each do |item|
+        row = []
+        item_fields.each do |key|
+          if key == "collection_id"
+            row << item.collection.division
+          else
+            row << item.attributes.values_at(key)[0]
+          end
+        end
+        item.preparations.each do |prep|
+          preparations_fields.each do |prep_key|
+            row << prep.attributes.values_at(prep_key)[0]
+          end
+        end
+        item.identifications.each do |identification|
+          if identification.current
+            identifications_fields.each do |id_key|
+              row << identification.attributes.values_at(id_key)[0]
+            end
+          end
+        end
+        csv << row
+      end
+    end
+  end
 end
