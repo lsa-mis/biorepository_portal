b2ff9193486bb9ef66e71ea3f941399ac55204fd
Margarita Barvinok <brita@umich.edu>
2025-07-15 12:56:46 -0400

    add set_announcement

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,17 +1,14 @@
 class AnnouncementsController < ApplicationController
+  before_action :set_announcement, only: [:edit, :update]
   def index
     authorize Announcement
     @announcements = Announcement.order(:location)
   end
 
   def edit
-    @announcement = Announcement.find(params[:id])
-    authorize @announcement
   end
 
   def update
-    @announcement = Announcement.find(params[:id])
-    authorize @announcement
     if @announcement.update(announcement_params)
       redirect_to announcements_path, notice: "Announcement updated"
     else
@@ -20,6 +17,11 @@ class AnnouncementsController < ApplicationController
   end
 
   private
+
+  def set_announcement
+    @announcement = Announcement.find(params[:id])
+    authorize @announcement
+  end
 
   def announcement_params
     params.require(:announcement).permit(:content, :displayed)
