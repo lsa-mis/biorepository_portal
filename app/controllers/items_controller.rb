b27a83b78ac0c3985dd969fa50232c8e97dffd3b
Margarita Barvinok <brita@umich.edu>
2025-07-18 20:59:04 -0400

    refactor

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -218,23 +218,35 @@ class ItemsController < ApplicationController
     end
 
     def transform_quick_search_params
-      quick_search_param = ""
-      if flash[:quick_search_q].is_a?(Hash) && flash[:quick_search_q].key?("country_case_insensitive_or_state_province_case_insensitive_or_identifications_scientific_name_or_identifications_vernacular_name_cont")
-        quick_search_param = flash[:quick_search_q]["country_case_insensitive_or_state_province_case_insensitive_or_identifications_scientific_name_or_identifications_vernacular_name_cont"]
-      end
+      quick_search_param = extract_quick_search_param
       return unless quick_search_param.present?
-      if quick_search_param.is_a?(String)
-        params[:q] = ActionController::Parameters.new({:groupings =>{"0" => {
-          "identifications_scientific_name_i_cont_any" => [quick_search_param],
-          "identifications_vernacular_name_i_cont_any" => [quick_search_param],
-          "m" => "or"
-        }}}).permit!
-        if @countries.flatten.any?(quick_search_param.downcase)
-          params[:q]["country_case_insensitive_in"] = [quick_search_param.downcase]
-        end
-        if @states.flatten.any?(quick_search_param.downcase)
-          params[:q]["state_province_case_insensitive_in"] = [quick_search_param.downcase]
-        end
+      construct_search_params(quick_search_param)
+      add_country_and_state_filters(quick_search_param)
+    end
+
+    def extract_quick_search_param
+      return unless flash[:quick_search_q].is_a?(Hash)
+      flash[:quick_search_q]["country_case_insensitive_or_state_province_case_insensitive_or_identifications_scientific_name_or_identifications_vernacular_name_cont"]
+    end
+
+    def construct_search_params(quick_search_param)
+      params[:q] = ActionController::Parameters.new({
+        groupings: {
+          "0" => {
+            "identifications_scientific_name_i_cont_any" => [quick_search_param],
+            "identifications_vernacular_name_i_cont_any" => [quick_search_param],
+            "m" => "or"
+          }
+        }
+      }).permit!
+    end
+
+    def add_country_and_state_filters(quick_search_param)
+      if @countries.flatten.any?(quick_search_param.downcase)
+        params[:q]["country_case_insensitive_in"] = [quick_search_param.downcase]
+      end
+      if @states.flatten.any?(quick_search_param.downcase)
+        params[:q]["state_province_case_insensitive_in"] = [quick_search_param.downcase]
       end
     end
 
