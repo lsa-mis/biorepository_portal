5b15d39d17e99032e3441ef92d63d3eea1cedc95
Margarita Barvinok <brita@umich.edu>
2025-09-01 18:54:06 -0400

    check available items

--- app/controllers/loan_requests_controller.rb
+++ app/controllers/loan_requests_controller.rb
@@ -65,7 +65,7 @@ class LoanRequestsController < ApplicationController
   def send_loan_request
     @shipping_address = Address.find(params[:shipping_address_id])
 
-    if @checkout.nil? || @checkout.requestables.empty?
+    if @checkout.nil? || @checkout.requestables.available.empty?
       flash[:alert] = "No items in checkout."
       redirect_to root_path
       return
