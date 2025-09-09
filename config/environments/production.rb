4e4147da040d96e87f833e3fdeadcf43e6fa4723
Margarita Barvinok <brita@umich.edu>
2025-09-09 19:48:38 -0400

    put host into env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://spaceready.lsa.umich.edu/'
+  host = ENV['HOST']
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
