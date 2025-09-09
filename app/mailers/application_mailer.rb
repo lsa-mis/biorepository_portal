6fb57e80b0b725a749b82ba8c2570f274835fa33
Margarita Barvinok <brita@umich.edu>
2025-09-09 15:59:06 -0400

    edit default mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,18 @@
 class ApplicationMailer < ActionMailer::Base
   prepend_view_path "app/views/mailers"
-  default from: "lsa-biorepository-super-admins@umich.edu"
-  default to: "lsa-biorepository-super-admins@umich.edu"
+  default from: "no-reply@biorepository.lsa.umich.edu",
+          reply_to: reply_to_email,
+          to: "lsa-biorepository-super-admins@umich.edu"
   layout "mailer"
+
+  private
+
+  def reply_to_email
+    if GlobalPreference.find_by(name: "generic_contact_email")&.value.present? 
+      GlobalPreference.find_by(name: "generic_contact_email").value
+    else
+      'lsa-biorepository-super-admins@umich.edu'
+    end
+  end
+  
 end
