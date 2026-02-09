9a57e839ff86107f6ea6868fd0ad12412b0e68ec
Margarita Barvinok <brita@umich.edu>
2026-02-09 13:02:42 -0500

    edit initializer

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,12 +1,13 @@
 # Bullet configuration for N+1 query detection
 if defined?(Bullet)
   Bullet.enable        = true
-  Bullet.alert         = true
+  Bullet.alert         = Rails.env.development?
   # Bullet.alert = false # Disable browser alerts (can be annoying)
   Bullet.console = true # Print in server console
   Bullet.rails_logger = true # Add to Rails.logger
   Bullet.bullet_logger = true # Log to bullet.log file
-  Bullet.add_footer = true # Add footer with detected issues
+  Bullet.add_footer = Rails.env.development? # Add footer with detected issues
+  Bullet.raise         = Rails.env.test? # raise an error if n+1 query occurs
   
   # Disable specific checks if needed
   # Bullet.unused_eager_loading_enable = false
