842c040672670bb44845793badcc003bb3ddcb5a
Margarita Barvinok <brita@umich.edu>
2026-02-23 23:54:32 -0500

    change the check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   include Pundit::Authorization
-  if Rails.env.production?
+  unless Rails.env.development?
     rescue_from StandardError, with: :render_500
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
   end
