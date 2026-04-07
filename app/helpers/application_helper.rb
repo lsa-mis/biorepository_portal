36bbfa3ffd3babc42cc385e7d442b3281491c02c
Margarita Barvinok <brita@umich.edu>
2026-04-07 10:53:21 -0400

    add another method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -205,13 +205,17 @@ module ApplicationHelper
       LdapLookup.is_member_of_group?(uniqname, group)
     end
   end
-  
+
+  def get_uniqname_from_email(email)
+    email.split("@").first
+  end
+
   def user_allowed_to_edit?(saved_search)
     return false unless user_signed_in?
     return true if is_super_admin?
     if saved_search.global && is_admin?
-      created_by_uniqname = saved_search.user.email.split("@").first
-      current_user_uniqname = current_user.email.split("@").first
+      created_by_uniqname = get_uniqname_from_email(saved_search.user.email)
+      current_user_uniqname = get_uniqname_from_email(current_user.email)
       user_groups = admin_groups_for_uniqname(current_user_uniqname)
       creator_groups = admin_groups_for_uniqname(created_by_uniqname)
       (user_groups & creator_groups).any?
